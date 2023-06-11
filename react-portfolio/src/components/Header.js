b581edc0533ad2dccb497fc981affea9214b8170
frankolms <frankolms@gmail.com>
2023-06-11 19:18:24 -0400

    finished styling header

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="my-name">
+        <h1>Frank Olmsted</h1>
+      </div>
+      <div className="links">
+        <a href="#">About Me</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Contact</a>
+        <a href="#">Resume</a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
