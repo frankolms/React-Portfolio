db7c9950106e2a967e5480898ea81bb517332a47
frankolms <frankolms@gmail.com>
2023-06-17 16:43:14 -0400

    began working on dynamically rendering projects in portfolio section

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Project() {
-  return <div>My Projects</div>;
+function Project(props) {
+  return (
+    <div>
+      <div>{props.data.title}</div>
+      <img src={props.data.image} alt={props.data.altText} />
+    </div>
+  );
 }
 
 export default Project;
--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -2,7 +2,22 @@ import React from "react";
 import Project from "../Project";
 
 function Portfolio() {
-  return <div>My Projects</div>;
+  const projects = [
+    {
+      title: "PokeWiki",
+      website: "https://tcunningham203.github.io/Etsy-Pokedex/",
+      image: "./images/Screenshot (81).png",
+      altText: "PokeWiki Logo",
+    },
+  ];
+
+  return (
+    <div>
+      {projects.map((project) => (
+        <Project data={project} />
+      ))}
+    </div>
+  );
 }
 
 export default Portfolio;
