7d46d091a1beef98ab1b12865588268160f1e345
frankolms <frankolms@gmail.com>
2023-06-19 21:02:35 -0400

    added links to footer links

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -3,9 +3,18 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p className="footer-item">Email</p>
-      <p className="footer-item">LinkedIn</p>
-      <p className="footer-item">GitHub</p>
+      <a href="https://twitter.com/olmsted_frank" className="footer-item">
+        Twitter
+      </a>
+      <a
+        href="https://www.linkedin.com/in/frank-olmsted-b4464b276/"
+        className="footer-item"
+      >
+        LinkedIn
+      </a>
+      <a href="https://github.com/frankolms" className="footer-item">
+        GitHub
+      </a>
     </footer>
   );
 }
