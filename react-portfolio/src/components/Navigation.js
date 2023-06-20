f41aa9c2b76e6a40b2df0f8647556fd39a856023
frankolms <frankolms@gmail.com>
2023-06-19 20:56:28 -0400

    added highlighting effect to nav bar links

--- react-portfolio/src/components/Navigation.js
+++ react-portfolio/src/components/Navigation.js
@@ -5,28 +5,36 @@ function Navigation({ renderedPage, handlePageRender }) {
     <nav className="links">
       <a
         href="#about-me"
-        className="section-links"
+        className={
+          renderedPage === "AboutMe" ? "rendered-page" : "section-links"
+        }
         onClick={() => handlePageRender("AboutMe")}
       >
         About Me
       </a>
       <a
         href="#portfolio"
-        className="section-links"
+        className={
+          renderedPage === "Portfolio" ? "rendered-page" : "section-links"
+        }
         onClick={() => handlePageRender("Portfolio")}
       >
         Portfolio
       </a>
       <a
         href="#contact"
-        className="section-links"
+        className={
+          renderedPage === "Contact" ? "rendered-page" : "section-links"
+        }
         onClick={() => handlePageRender("Contact")}
       >
         Contact
       </a>
       <a
         href="#resume"
-        className="section-links"
+        className={
+          renderedPage === "Resume" ? "rendered-page" : "section-links"
+        }
         onClick={() => handlePageRender("Resume")}
       >
         Resume
